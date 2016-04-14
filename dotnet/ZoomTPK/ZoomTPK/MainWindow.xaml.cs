e07b8fa822c8ebdda9593670768d45fa2196e4bf
david-chambers <davidchambers14@gmail.com>
2016-04-14 16:59:12 -0500

    added a tpk to resources

--- dotnet/ZoomTPK/ZoomTPK/MainWindow.xaml.cs
+++ dotnet/ZoomTPK/ZoomTPK/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Esri.ArcGISRuntime.Controls;
+using Esri.ArcGISRuntime.Layers;
 using System;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Windows;
 
@@ -8,9 +10,24 @@ namespace ZoomTPK
 {
     public partial class MainWindow : Window
     {
+
+        ArcGISLocalTiledLayer tiledLyr;
+        String appPath, tpkFilePath, tpkFullPath;
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //Get the path to the .tpk in the Resources folder
+            appPath = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+            tpkFilePath = Path.Combine(appPath, "Resources");
+            tpkFullPath = Path.Combine(tpkFilePath, "CharlotteMap.tpk");
+
+
+            tiledLyr = new ArcGISLocalTiledLayer(tpkFullPath);
+
+            MyMapView.Map.Layers.Add(tiledLyr);
         }
 
         private void MyMapView_LayerLoaded(object sender, LayerLoadedEventArgs e)
